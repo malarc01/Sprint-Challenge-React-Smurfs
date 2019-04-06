20ebdeb7b1856451bb96be76ff553e2d2886a6b6
Husot <cobalt5alcon@gmail.com>
2019-04-06 13:47:30 -0400

    AXIOS request to POST done

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
@@ -12,6 +12,15 @@ class SmurfForm extends Component {
 
   addSmurf = event => {
     event.preventDefault();
+    console.log('addSmurf function invoked');
+    let smurfObject={
+      name:this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    }
+    axios.post('http://localhost:3333/smurfs',smurfObject)
+    .then(res=>console.log(res))
+    .catch(err=>console.log(err))
     // add code to create the smurf using the api
 
     this.setState({
